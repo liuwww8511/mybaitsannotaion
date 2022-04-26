41add699ad93a701f93162dfa5ee9011857c8beb
Administrator <liuwww8511@qq.com>
2022-04-26 22:26:33 +0800

    Changes

---
+++ src/main/java/com/vivi/mybatis/dao/EmployeeMapperDynamicSQL.java
@@ -0,0 +1,12 @@
+package com.vivi.mybatis.dao;
+
+import com.vivi.mybatis.beans.Employee;
+
+import java.util.List;
+
+public interface EmployeeMapperDynamicSQL {
+
+    public List<Employee> getEmpsByConditionIfWhere(Employee Condition);
+
+    public void  updateEmpByConditionSet(Employee Condition);
+}
